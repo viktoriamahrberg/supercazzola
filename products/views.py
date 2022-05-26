51e8881e2991deb74d1d2a40084eb4a2c570cb76
Vikmah <viktoria.mahrberg@gmail.com>
2022-05-26 13:24:59 +0000

    Add search filter to views

--- products/views.py
+++ products/views.py
@@ -1,13 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 def all_products(request):
     """ View to all products, including sorting and searching """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You did't search for anything")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+
 
     context = {
-        'products' : products
+        'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ View to each detailed product page """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product' : product
+    }
+
+    return render(request, 'products/product_detail.html', context)
