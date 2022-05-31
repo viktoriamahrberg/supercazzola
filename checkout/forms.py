3dc9cf36eb34728102546dbab1115fb39d2896a6
Vikmah <viktoria.mahrberg@gmail.com>
2022-05-31 16:30:19 +0000

    Add order form to checkout

---
+++ checkout/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from .model import Order
+
+
+class Orderform(forms.modelForm):
+    class Meta:
+        model = Order
+        fields = ('full_name', 'email', 'phone_number', 
+                    'street_address', 'optional_address', 'country',
+                    'postcode', 'city')
+
+    def __init__(self, *args, **kwargs):
+        """
+        Calls original init method to set up form as default
+        and then renders it with set placeholder classes and auto-focus
+        on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'street_address': 'Street Address',
+            'optional_address': 'Optional Street Address',
+            'postcode': 'Postal Code',
+            'city': 'City',
+        }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+                self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+                self.fields[field].label = False
