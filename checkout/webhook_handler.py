29d289230c41c16085ff5931d0160d89332f7a9e
Vikmah <viktoria.mahrberg@gmail.com>
2022-06-02 18:55:44 +0000

    Set up webhooks listeners from stripe

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -16,3 +16,19 @@ class StripeWH_Handler:
         return HttpResponse(
             content=f'Unhandled webhook received: {event["type"]}',
             status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle payment intent succeeded event from Stripe
+        """
+        return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_failed(self, event):
+        """
+        Handle payment intent payment failed event from Stripe
+        """
+        return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
