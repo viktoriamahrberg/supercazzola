d48718e17c8990333c41034162064ce75311d993
Vikmah <viktoria.mahrberg@gmail.com>
2022-06-05 19:57:25 +0000

    Add delivery information on users profile

---
+++ profiles/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    """
+    User Profile in My Account
+    """
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Calls original init method to set up form as default
+        and then renders it with set placeholder classes and auto-focus
+        on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_phone_number': 'Phone Number',
+            'default_street_address': 'Street Address',
+            'default_optional_address': 'Optional Street Address',
+            'default_postcode': 'Postal Code',
+            'default_city': 'City',
+        }
+
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+                self.fields[field].widget.attrs['class'] = 'border-black rounded-0'
+                self.fields[field].label = False
--- profiles/models.py
+++ profiles/models.py
@@ -16,7 +16,7 @@ class UserProfile(models.Model):
     default_optional_address = models.CharField(max_length=80, null=True, blank=True)
     default_city = models.CharField(max_length=40, null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
-    default_country = CountryField(blank_label='Country *', null=True, blank=True) 
+    default_country = CountryField(blank_label='Country', null=True, blank=True) 
 
     def __str__(self):
         return self.user.username
