764e94171104275bcdad51b040366fb53cf86252
Vikmah <viktoria.mahrberg@gmail.com>
2022-06-06 11:59:15 +0000

    Add webhook handler to add users data to profile template

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile
 
 import json
 import time
@@ -41,6 +42,20 @@ class StripeWH_Handler:
             if value == "":
                 shipping_details.address[field] = None
 
+        # Webhook handler to update profile info if save_info was checked
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_street_address = shipping_details.address.line1
+                profile.default_optional_address = shipping_details.address.line2
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_city = shipping_details.address.city
+                profile.save()
+
         # Assuming the order doesn't exist in our database
         order_exists = False
         attempt = 1
@@ -74,6 +89,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
