d0378a1803954332cd4bd6967bfb2b3c68dbd7c6
Vikmah <viktoria.mahrberg@gmail.com>
2022-06-06 12:44:48 +0000

    Add email confirmation to webhook handler

--- supercazzola/settings.py
+++ supercazzola/settings.py
@@ -191,6 +191,7 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'orders@supercazzola.com'
 
 
 # Default primary key field type
