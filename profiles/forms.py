b437491ffde04bc3b26ca66f91047aec53a81449
Vikmah <viktoria.mahrberg@gmail.com>
2022-06-06 09:15:59 +0000

    Add update form to profile view and successmessage to show without shopping cart when doing so

--- profiles/forms.py
+++ profiles/forms.py
@@ -29,9 +29,11 @@ class UserProfileForm(forms.ModelForm):
         for field in self.fields:
             if field != 'default_country':
                 if self.fields[field].required:
+
                     placeholder = f'{placeholders[field]} *'
                 else:
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
                 self.fields[field].widget.attrs['class'] = 'border-black rounded-0'
                 self.fields[field].label = False
+                self.fields['default_country'].label = False
