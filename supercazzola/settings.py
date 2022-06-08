d44fad7931a6128df8b022ea69e82bae347edade
Vikmah <viktoria.mahrberg@gmail.com>
2022-06-08 09:52:49 +0000

    Deployment part 2

--- supercazzola/settings.py
+++ supercazzola/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-vb-1#bt-ua0$%36c%(26^jf&v-g$j1kc&d@hs1m_j)iz7v=9j+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['supercazzola.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -136,12 +136,17 @@ WSGI_APPLICATION = 'supercazzola.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 
