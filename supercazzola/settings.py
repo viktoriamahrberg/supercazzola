47923be3441deabdb85c504be7667bd2ea6622ac
Vikmah <viktoria.mahrberg@gmail.com>
2022-07-27 10:51:04 +0000

    Clean up code navbar and base.html

--- supercazzola/settings.py
+++ supercazzola/settings.py
@@ -139,6 +139,7 @@ else:
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
+    
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
